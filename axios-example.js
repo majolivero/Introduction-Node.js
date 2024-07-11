f97762e6fc73488afee00f058e9066b2568b9864
majolivero <majolivero28@gmail.com>
2024-07-11 17:21:25 -0500

    Elimine carpeta node modules

---
+++ axios-example.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+axios
+  .get("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => console.log(response.data))
+  .catch((error) => console.error(error));
+
+  
